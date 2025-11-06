2a80193ce388f826b1dd956a9f08bfd9178402e2
Gabriel Rączkowski <119063185+NotASeal23@users.noreply.github.com>
2025-11-06 18:31:17 +0100

    Create zad6.sql

---
+++ 03_praca_z_kolekcjami_geometrii/03_here/zad6.sql
@@ -0,0 +1,11 @@
+CREATE TABLE public.droga AS
+ SELECT ST_MakeLine(
+(SELECT geom FROM public.input_points WHERE id = 1),
+(SELECT geom FROM public.input_points WHERE id = 2)
+) AS line_geom;
+
+
+SELECT *
+FROM droga d,
+     c_t2019_kar_street_node s
+WHERE ST_DWithin(d.line_geom,ST_Transform(s.geom,3068),200);
